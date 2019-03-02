fc13c894897201bdc2fc9a9010bd9d592e8c78fc
Wian <wian.2701@gmail.com>
2019-03-01 23:09:24 -0400

    bug do cadastro com campos em branco

--- OFICIAL - Projeto-LT/conexao.php
+++ OFICIAL - Projeto-LT/conexao.php
@@ -18,11 +18,17 @@ $senha = md5($senha);
 
 $sql = "INSERT INTO usuario (nome,email,senha) VALUES(?,?,?)";
 
+
 $sqlprep = $conexao->prepare($sql);
 $sqlprep->bind_param("sss", $nome,$email,$senha);
 
 $sql2 = "SELECT * FROM usuario WHERE email = '$email'";
 $insert = mysqli_query($conexao,$sql2) or die("erro");
+if ($nome = $_POST['nome']|| $email = $_POST['email'] || $senha = $_POST['senha'] == ""){
+    echo  "<script> alert('Alguns campos estão em branco, por favor volte e tente novamente!') </script>";
+    header("Location:cadastro.html");
+  
+}
 if (mysqli_num_rows($insert) < 1) {
 
     $sqlprep->execute();
